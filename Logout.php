58ed0f09681d39907f866bee23361036fb358068
OmprakashD2023 <109577715+OmprakashD2023@users.noreply.github.com>
2022-10-15 17:58:16 +0530

    Delete Logout.php

--- Logout.php
+++
@@ -1,9 +0,0 @@
-<?php
-require('Admin/connection.inc.php');
-require('Admin/functions.inc.php');
-unset($_SESSION['USER_LOGIN']);
-unset($_SESSION['USER_ID']);
-unset($_SESSION['USER_NAME']);
-header('location:index-0.php');
-die();
-?>
